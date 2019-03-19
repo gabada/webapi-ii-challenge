e75fa4e3566b518197daf222544f400921a021b5
Gill Abada <gill@callifi.com>
2019-03-19 08:17:19 -0400

    complete GET using ID.

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -15,4 +15,23 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  db.findById(id)
+    .then(post => {
+      if (post.id) {
+        res.status(200).json(post);
+      } else {
+        return res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      }
+    })
+    .catch(err => {
+      return res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' });
+    });
+});
+
 module.exports = router;
