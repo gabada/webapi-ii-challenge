3c4b398e92d83a14ce8d3992bc07ba50a9329dbe
Gill Abada <gill@callifi.com>
2019-03-19 07:58:41 -0400

    finish GET endpoint.

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const db = require('../data/db.js');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.status(200).send('test get');
+  db.find()
+    .then(posts => {
+      res.status(200).json(posts);
+    })
+    .catch(err => {
+      return res
+        .status(500)
+        .json({ error: 'The posts information could not be retrieved.' });
+    });
 });
 
 module.exports = router;
